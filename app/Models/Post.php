f0cbfd66dc0c8fe207ea548bb802117bab62a796
Rafał Pruś <rafalprues@gmail.com>
2023-07-13 09:54:24 +0200

    small improvements

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -31,6 +31,17 @@ class Post
         return static::all()->firstWhere('slug', $slug);
     }
 
+    public static function findOrFail($slug)
+    {
+        $post = static::find($slug);
+
+        if (! $post) {
+            throw new ModelNotFoundException();
+        }
+
+        return $post;
+    }
+
     public static function all()
     {
             $files = File::files(resource_path("posts"));
