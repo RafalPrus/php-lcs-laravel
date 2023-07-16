ce47e6429984075e5f3fedb03b1e01d1076bcd60
Rafał Pruś <rafalprues@gmail.com>
2023-07-16 09:26:59 +0200

    Merge category & search queries

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -10,7 +10,7 @@
     <x-dropdown-item href="/" :active="false">All</x-dropdown-item>
     @foreach ($categories as $category)
         <x-dropdown-item
-            href="/?category={{ $category->slug }}"
+            href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category')) }}"
             :active="false"
         >{{ ucwords($category->name) }}</x-dropdown-item>
     @endforeach
