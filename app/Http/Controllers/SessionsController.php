2b755bef06bc8ac0717257beedc9cf739c053bfc
Rafał Pruś <rafalprues@gmail.com>
2023-07-17 15:08:33 +0200

    Login page

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -6,6 +6,33 @@ use Illuminate\Http\Request;
 
 class SessionsController extends Controller
 {
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+
+        ]);
+
+        if (auth()->attempt($attributes)) {
+            session()->regenerate();
+            
+            return redirect('/')->with('success', 'Welcome back :) !');
+        }
+
+        return back()
+            ->withInput()
+            ->withErrors([
+            'email' => 'The provided email or password is incorrect',
+        ]);
+
+
+    }
     public function destroy()
     {
         auth()->logout();
